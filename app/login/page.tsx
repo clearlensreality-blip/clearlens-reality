2c69150515c847deb80eea1bf3d6ef03dbf94b9a
Jason <clearlensreality@gmail.com>
2026-04-01 18:37:32 +0100

    Updated navbar with About and Contact Us

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import Button from "@/components/button";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center space-y-20">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center pt-32 space-y-20">
 
       {/* HERO */}
       <section className="space-y-4 text-center">
@@ -18,22 +18,22 @@ export default function LoginPage() {
       </section>
 
       {/* FORM */}
-      <section className="w-full p-8 rounded-xl bg-gradient-to-br from-black via-neutral-900 to-blue-950 border border-blue-900/40 shadow-lg shadow-black/40 max-w-xl">
+      <section className="w-full p-8 rounded-xl bg-blue-600 border border-blue-900/40 shadow-lg shadow-black/40 max-w-xl">
         <form className="space-y-6">
 
           <div>
-            <label className="block mb-2 text-gray-300">Email</label>
+            <label className="block mb-2 text-gray-200">Email</label>
             <input
               type="email"
-              className="w-full p-3 rounded bg-neutral-800 border border-neutral-700"
+              className="w-full p-3 rounded bg-neutral-900/40 border border-neutral-700"
             />
           </div>
 
           <div>
-            <label className="block mb-2 text-gray-300">Password</label>
+            <label className="block mb-2 text-gray-200">Password</label>
             <input
               type="password"
-              className="w-full p-3 rounded bg-neutral-800 border border-neutral-700"
+              className="w-full p-3 rounded bg-neutral-900/40 border border-neutral-700"
             />
           </div>
 
